05833901cb950f574cccec930736b76d15c043ee
raulduke2022 <sergeytersahakyan@gmail.com>
2023-08-01 10:43:24 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -25,12 +25,4 @@ export default defineConfig({
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
-    build: {
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-                auth: 'resources/js/auth.js',
-            },
-        },
-    },
 });
