ac515f57a76f1feba7903ea80d72f97e32981f87
raulduke2022 <sergeytersahakyan@gmail.com>
2023-08-02 17:28:56 +0300

    new

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 import '../sass/app.scss';
 import '../css/app.css';
-import '../css/redirect.css';
 
 import { createApp } from 'vue';
 const app = createApp({});
--- resources/views/payeer/payment.blade.php
+++ resources/views/payeer/payment.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/redirect.css'])
+    <link rel="stylesheet" href="../../css/redirect.css">
     <title>Market</title>
 </head>
 <body>
