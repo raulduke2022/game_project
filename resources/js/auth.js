81164fa1370374c1aa4b3071f5446aed65381cd0
raulduke2022 <sergeytersahakyan@gmail.com>
2023-08-02 17:25:46 +0300

    new

--- resources/js/auth.js
+++ resources/js/auth.js
@@ -1,8 +1,7 @@
 import './bootstrap';
 import '../sass/app.scss';
 import '../css/app.css';
-import '../css/redirect.css.css';
-
+import '../css/redirect.css';
 
 import { createApp } from 'vue';
 const app = createApp({});
