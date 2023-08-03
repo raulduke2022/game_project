8a983efd405ddf40e7672a7ad7c1f329a46d6d96
raulduke2022 <sergeytersahakyan@gmail.com>
2023-08-03 17:53:26 +0300

    new

--- app/Http/Controllers/PayeerController.php
+++ app/Http/Controllers/PayeerController.php
@@ -124,12 +124,12 @@ class PayeerController extends Controller
 
     public function success()
     {
-        return redirect()->back();
+        return redirect()->route('market');
     }
 
     public function fail()
     {
-        return redirect()->back();
+        return redirect()->route('market');
     }
 
 }
