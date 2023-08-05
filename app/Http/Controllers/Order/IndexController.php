05808dca4075ecdb8bfe2205676db4d45d4ad4d5
elik <elik@example.com>
2023-08-05 17:35:23 +0300

    new

--- app/Http/Controllers/Order/IndexController.php
+++ app/Http/Controllers/Order/IndexController.php
@@ -14,9 +14,9 @@ class IndexController extends Controller
         $records = Order::query();
 
         if ($isDone === 'true') {
-            $records->where('status', 'success');
+            $records->where('is_done', true);
         } elseif ($isDone === 'false') {
-            $records->whereNotIn('status', ['success'])->orWhereNull('status');;
+            $records->whereNotIn('is_done', false);
         }
         $orders = $records->with('game')->orderBy('created_at', 'desc')->paginate(10); //eager loading
         $orders->appends(['is_done' => $isDone]);
